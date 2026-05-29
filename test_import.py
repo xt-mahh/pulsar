fd7ade6868c59a70c6c203a594eef02f124677ee
xt-mahh <xt_mahh@outlook.com>
2026-05-29 17:01:19 +0800

    chore: cleanup - remove test files, update gitignore

--- test_import.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-sys.path.insert(0, 'src')
-try:
-    from pulsar.execution.tools.base import BaseTool
-    print("base OK")
-except Exception as e:
-    import traceback
-    traceback.print_exc()
